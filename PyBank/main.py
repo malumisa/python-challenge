f0b2104b2108291d5965a058fb29e6e4e3a497f5
malumisa <111699427+malumisa@users.noreply.github.com>
2022-11-10 12:20:32 -0800

    work in progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,40 +3,78 @@ import os
 import csv
 
 # path to collect data from the Resources folder
-budgdet_csv=os.path.join("..", "Resources", "budget_data.csv")
+budget_csv=os.path.join( "Resources", "budget_data.csv")
 
-#List to store data
-Date = []
-ProfitLosses =[]
+# Results to output
+total_months = 0
+month_of_change = []
+net_change_list = []
+ProfitLosses = 0
+Greatest_Increase_in_Profits = []
+Greatest_Decrease_in_Profits =  []
+Average_Change = []
 
 
 # With open  (udemy_csv, encoding ='utf-8') as csv file
-with open (budgdet_csv) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open (budget_csv, encoding='utf') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+#Reading the header row
+    header = next(csvreader)
+
     for row in csvreader:
-        print(row) 
-
-
         #Add Date
-        Date.append(row[0])
+        if row[0] == Date:
+            print(len(row[0]))
 
-        # Add profit/Losses
-        ProfitLosses.append(row[1])
+  # Track the total number of months
+    length = len(Date)
+    print(length)
 
+# The net total amount of "profit/Losses" over hte entire period
 
+    ProfitLosses(row[1])
+ProfitLosses += int(row[1])
+        #print(int(row([1])))
+    #print(ProfitLosses)
+         
+ # The changes in "Profit/Losses" over the entire period, and then the average changes       
+changes = ProfitLosses (i+1) - ProfitLosses(i)
 
-
-#Calculating the net total amount of "Profit/Losses" over the entire period
-Total = sum(int(ProfitLoss))
-
-#Determining the changes in "Profit/Losses" over th entire period
-#x = [1, 3, 5, 8]
-#for i in range(1, len(x)):
-    #print(x[i] - x[i-1])
-
+def average (changes)
+    length = len(changes)
+    total = 0.0
+    for number in changes:
+        total += number
+    return total / length
 
 # The greatest increase in profits (date and amount) over the entire period
+ProfitLosses = int(row[1])
+print(max(ProfitLosses))
+       
+    
+ # The greatest decrease in profits (date and amount) over the entire period
+ProfitLosses = int(row[1])
+print(max(ProfitLosses))
+    
+        # Calculate the greatest decrease
+
+# Calculate the Average Net Change
+
+# Export the results to text file
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(f"Financial Analysis\n")
+    txt_file.write(f"----------------------------\n")
+    txt_file.write(f"Total Months: {total_months}\n")
+    txt_file.write(f"Total: ${total_net}\n")
+    txt_file.write(f"Average  Change: ${net_monthly_avg}\n")
+    txt_file.write(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n")
+    txt_file.write(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+
+    Date = str(budget_data[0])
+    ProfitLosses = int(budget_data[1])
 
 
 
-# The greatest decrease in profits (date and amount) over the entire period
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,38 +1,41 @@
 import os
 import csv
 
-election_csv =os.path.join("..", "Resources", "election_data.csv")
+# path to collect data from the Resources folder
+election_csv =os.path.join('..', 'Resources', 'election_data.csv')
 
-#Lists to store
-BallotID = []
-County = []
-Candidate = []
+# Results to output
+total_votes_cast = 0
+list_candidates_with_votes = []
+percentage_votes_per_candidate = []
+total_votes_per_candidate = []
+winner_based_on_vote = []
 
 
-# with open (udemy_scv, encoding= 'utf-8') as csvfile:
-with open(election_csv) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    for row in csvreader:
-        # Add BallotID
-        BallotID.append(row[0])
+# with open (csv, encoding = 'utf -8')  as csv file
+with open (election_csv, encoding = 'utf') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
 
-        #Add County
-        County.append(row[1])
+# Reading the header row
+header = next(csvreader)
 
-        #Add Candidate
-        Candidate.append(row[2])
+for row in csvreader:
+    if row[0] == BallotID:
+
+def print_election(election_data):
+    BallotID = int(election_data [0])
+    County = str(election_data [1])
+    Candidate = str(election_data [2])
 
 # The total number of votes cast
-
+Total_votes_cast = len(BallotID)
 
 # Complete list of candidates who received votes
-
+Candidate_list = list(Candidate)
 
 # Percentage of votes each candidate won to 3 decimal places
-percent= round(int(row[2] /int(row[1]) * 100,3)
+percent= round(int([2] /int([1]) * 100,3)
 percentage_votes.append(str(percent) + "%"))
-
-
 
 #Total number of votes each candodate won
 
@@ -40,4 +43,12 @@ percentage_votes.append(str(percent) + "%"))
 #Winner of the election based on popular votes
 
 
-
+# Export the electoion results to text file
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(f"Election Results\n")
+    txt_file.write(f"----------------------------\n")
+    txt_file.write(f"Total number of votes cast : {}\n")
+    txt_file.write(f"TList of candidates hwo received votes: ${}\n")
+    txt_file.write(f"Percentage of votes per candidate: ${total_votes_per_candidate[0]}) (${candidate_list[1]} \n")
+    txt_file.write(f"Total number of votes per candidate: {total_votes_per_candidate[0]}) (${candidate_list[1]}\n")
+    txt_file.write(f"Winner: {popular_vote})\n")
