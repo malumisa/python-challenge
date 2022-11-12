c8ce261ff9c32a5831a60139b847a6f8b0e8f4aa
malumisa <111699427+malumisa@users.noreply.github.com>
2022-11-11 16:49:50 -0800

    work in progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,24 @@
 # import os and csv
+
 import os
 import csv
 
 # path to collect data from the Resources folder
 budget_csv=os.path.join( "Resources", "budget_data.csv")
 
-# Results to output
-total_months = 0
+# Results to Financial Analysis output
+total_months = []
 month_of_change = []
 net_change_list = []
-ProfitLosses = 0
+Total_ProfitLoss = 0
 Greatest_Increase_in_Profits = []
 Greatest_Decrease_in_Profits =  []
 Average_Change = []
-
+counter = 0
+Previous_ProfitLoss = 1088983
+sum_of_changes = 0
+biggest_change =0
+smallest_change =0
 
 # With open  (udemy_csv, encoding ='utf-8') as csv file
 with open (budget_csv, encoding='utf') as csvfile:
@@ -22,58 +27,64 @@ with open (budget_csv, encoding='utf') as csvfile:
 #Reading the header row
     header = next(csvreader)
 
-    for row in csvreader:
-        #Add Date
-        if row[0] == Date:
-            print(len(row[0]))
+#Print the contents of each row
+    for Line in csvreader:
+        Date = Line [0]
+        ProfitLoss = int(Line[1])
+        counter = counter + 1
+        Total_ProfitLoss += int(Line[1])
+        changes =  (ProfitLoss - Previous_ProfitLoss)
+        Previous_ProfitLoss = ProfitLoss
+        sum_of_changes += changes
+        if changes > biggest_change:
+            biggest_change = changes
+        if  changes < smallest_change:
+            smallest_change = changes
 
-  # Track the total number of months
-    length = len(Date)
-    print(length)
 
-# The net total amount of "profit/Losses" over hte entire period
 
-    ProfitLosses(row[1])
-ProfitLosses += int(row[1])
-        #print(int(row([1])))
-    #print(ProfitLosses)
-         
+
+ # Track the total number of months
+    print(f"Total months {counter}")
+
+# The net total amount of "profit/Losses" over the entire period
+    print(f"Sum ProfitLoss {Total_ProfitLoss}") 
+        
  # The changes in "Profit/Losses" over the entire period, and then the average changes       
-changes = ProfitLosses (i+1) - ProfitLosses(i)
+    print(f"Sum of changes in Profit/Losses  {sum_of_changes}")     
+    print(f"Average changes {sum_of_changes/85}")
 
-def average (changes)
-    length = len(changes)
-    total = 0.0
-    for number in changes:
-        total += number
-    return total / length
 
 # The greatest increase in profits (date and amount) over the entire period
-ProfitLosses = int(row[1])
-print(max(ProfitLosses))
-       
+    print(f"Biggest Increase {biggest_change}")
+
+#  # The greatest decrease in profits (date and amount) over the entire period
+    print(f"Biggest Decrease {smallest_change}")
+# 
+# 
+# ProfitLosses = int(row[1])
+# print(f'[{int(max(ProfitLosses))}] {Date}')
     
- # The greatest decrease in profits (date and amount) over the entire period
-ProfitLosses = int(row[1])
-print(max(ProfitLosses))
-    
-        # Calculate the greatest decrease
-
-# Calculate the Average Net Change
-
-# Export the results to text file
-with open(file_to_output, "w") as txt_file:
-    txt_file.write(f"Financial Analysis\n")
-    txt_file.write(f"----------------------------\n")
-    txt_file.write(f"Total Months: {total_months}\n")
-    txt_file.write(f"Total: ${total_net}\n")
-    txt_file.write(f"Average  Change: ${net_monthly_avg}\n")
-    txt_file.write(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n")
-    txt_file.write(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+#         # Calculate the greatest decrease
 
 
-    Date = str(budget_data[0])
-    ProfitLosses = int(budget_data[1])
+# # save the output file path
+# financial_analysis = os.path.join("Financial Analysis")
+# # Calculate the Average Net Change
+
+# # Export the results to text file
+# with open(financial_analysis, "w") as txt_file:
+#     txt_file.write(f"Financial Analysis\n")
+#     txt_file.write(f"----------------------------\n")
+#     txt_file.write(f"Total Months: {total_months}\n")
+#     txt_file.write(f"Total: ${total_net}\n")
+#     txt_file.write(f"Average  Change: ${net_monthly_avg}\n")
+#     txt_file.write(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n")
+#     txt_file.write(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+
+#     Date = str(budget_data[0])
+#     ProfitLosses = int(budget_data[1])
 
 
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 # path to collect data from the Resources folder
-election_csv =os.path.join('..', 'Resources', 'election_data.csv')
+election_csv =os.path.join("Resources", 'election_data.csv')
 
 # Results to output
 total_votes_cast = 0
@@ -10,6 +10,9 @@ list_candidates_with_votes = []
 percentage_votes_per_candidate = []
 total_votes_per_candidate = []
 winner_based_on_vote = []
+counter = 0
+candidateList = []
+votes = 0
 
 
 # with open (csv, encoding = 'utf -8')  as csv file
@@ -17,25 +20,46 @@ with open (election_csv, encoding = 'utf') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
 
 # Reading the header row
-header = next(csvreader)
+    header = next(csvreader)
 
-for row in csvreader:
-    if row[0] == BallotID:
+#print the contents of each row
+    
+    for line in csvreader:
+        counter = counter + 1
+        candidate = str(line[2])
+        votes = str(line[1])
+        # Total_ProfitLoss += int(Line[1])
+        # changes =  (ProfitLoss - Previous_ProfitLoss)
+        # Previous_ProfitLoss = ProfitLoss
+        # sum_of_changes += changes
+        # if changes > biggest_change:
+        #     biggest_change = changes
+        # if  changes < smallest_change:
+        #     smallest_change = changes
 
-def print_election(election_data):
-    BallotID = int(election_data [0])
-    County = str(election_data [1])
-    Candidate = str(election_data [2])
+ # Total number of votes cast
+    print(f"Total Votes {counter}")
 
-# The total number of votes cast
-Total_votes_cast = len(BallotID)
+# Candidate List
+    print("f{candidate}") 
+        
+ # Number of votes eacg candidate won       
+    print(f"{candidate}, {votes}")
 
-# Complete list of candidates who received votes
-Candidate_list = list(Candidate)
+# # The greatest increase in profits (date and amount) over the entire period
+#     print(f"Biggest Increase {biggest_change}")
+
+# # The total number of votes cast
+# total_votes_cast = len(int(BallotID))
+# print(total_votes_cast)
+
+
+# # Complete list of candidates who received votes
+# #Candidate_list = list(Candidate)
 
 # Percentage of votes each candidate won to 3 decimal places
-percent= round(int([2] /int([1]) * 100,3)
-percentage_votes.append(str(percent) + "%"))
+#percent= round(int([2] /int([1]) * 100,3)
+#percentage_votes.append(str(percent) + "%"))
 
 #Total number of votes each candodate won
 
@@ -43,12 +67,14 @@ percentage_votes.append(str(percent) + "%"))
 #Winner of the election based on popular votes
 
 
+# save results to output file
+election_results = os.path.join("Election Results")
 # Export the electoion results to text file
-with open(file_to_output, "w") as txt_file:
+with open(election_results, "w") as txt_file:
     txt_file.write(f"Election Results\n")
     txt_file.write(f"----------------------------\n")
-    txt_file.write(f"Total number of votes cast : {}\n")
-    txt_file.write(f"TList of candidates hwo received votes: ${}\n")
+    txt_file.write(f"Total Votes : {total_votes_cast}\n")
+    txt_file.write(f"TList of candidates who received votes: ${total_votes_per_candidate}\n")
     txt_file.write(f"Percentage of votes per candidate: ${total_votes_per_candidate[0]}) (${candidate_list[1]} \n")
     txt_file.write(f"Total number of votes per candidate: {total_votes_per_candidate[0]}) (${candidate_list[1]}\n")
     txt_file.write(f"Winner: {popular_vote})\n")
